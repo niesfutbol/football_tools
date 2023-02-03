3c1b51202e1051a7dbcd679c8e357612c6e74b7c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-02 21:36:54 -0800

    🚧 Add private variables

--- R/diff_pts_diff_xgoal.R
+++ R/diff_pts_diff_xgoal.R
@@ -21,14 +21,22 @@ get_league_name_from_season <- function(season) {
   return(season$league[1])
 }
 
-League <- R6::R6Class("League", list(
-  names = NULL,
-  season = NULL,
-  league = NULL,
-  initialize = function(league, season, names) {
-    self$names <- names
-    self$league <- league %>%
-      left_join(season, by = c("match_id" = "id_match"))
-    self$season <- season
-  }
-))
+League <- R6::R6Class("League",
+  public = list(
+    names = NULL,
+    season = NULL,
+    league = NULL,
+    initialize = function(league, season, names) {
+      self$names <- names
+      self$league <- league %>%
+        left_join(season, by = c("match_id" = "id_match"))
+      self$season <- season
+    },
+    set_id_team = function(id_team) {
+      private$id_team <- id_team
+    }
+  ),
+  private = list(
+    id_team = NULL
+  )
+)
