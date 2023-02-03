7f3ad479c2936cc06f766c14229c119d10823191
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-02 20:34:11 -0800

    🔥Remove code of another repo

--- tests/testthat/test-diff_pts_diff_xgoal.R
+++ tests/testthat/test-diff_pts_diff_xgoal.R
@@ -1,22 +1,3 @@
-run_diff_pts_diff_xgoal <- function() {
-  league_season <- "135_2022"
-  directory <- "/workdir/tests/data"
-  src_file <- "/workdir/src/diff_pts_diff_xgoal.R"
-  command <- glue::glue("Rscript {src_file} -l {league_season} -d {directory}")
-  system(command)
-}
-
-describe("Run diff_pts_diff_xgoal", {
-  it("run", {
-    output_file <- "/workdir/tests/data/Inter_135_2022.jpg"
-    testtools::delete_output_file(output_file)
-    run_diff_pts_diff_xgoal()
-    expected_hash <- "d3135232327f5c696865e9479a382df3"
-    obtained_hash <- as.vector(tools::md5sum(output_file))
-    # expect_equal(obtained_hash, expected_hash)
-  })
-})
-
 describe("get_league_name_from_season", {
   it("Serie A", {
     path <- glue::glue("/workdir/tests/data/season_135_2022.csv")
