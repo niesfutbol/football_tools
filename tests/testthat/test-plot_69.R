65a8d8981e26c5b84699e997e04e35c11ffa13b6
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-08 20:24:05 -0800

    🛑🧪 Test `get_info_from_match()`

--- tests/testthat/test-plot_69.R
+++ tests/testthat/test-plot_69.R
@@ -4,11 +4,13 @@ all_matches <- read_csv("/workdir/tests/data/all_teams.csv", show_col_types = FA
 
 describe("get_info_from_match", {
   it("get the some info", {
-    match_id = 867986
+    match_id <- 867986
     expected <- tibble(
       match_id = c(867986),
       home_id = c(35),
-      away_id = c(39))
+      away_id = c(39)
+    )
     obtained <- get_info_from_match(all_matches, match_id)
+    expect_equal(obtained, expected)
   })
 })
