79d77379587f0f3f9d477a343c4b01182ea36a14
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-14 20:26:47 -0800

    🛑🧪 Test `select_match_to_predict()`

--- tests/testthat/test-prediction_from_multinom.R
+++ tests/testthat/test-prediction_from_multinom.R
@@ -14,4 +14,11 @@ describe("select_match_to_predict", {
     february <- read_csv("/workdir/tests/data/season_78_2022_february.csv", show_col_types = FALSE)
     expect_equal(obatined_matches, february)
   })
+  it("all at once", {
+    season <- read_csv("/workdir/tests/data/season_78_2022.csv", show_col_types = FALSE)
+    options <- list("round" = "22", "initial_date" = "2023-01-31", "final_date" = "2023-02-28")
+    obatined_matches <- select_match_to_predict(season, options)
+    round_22 <- read_csv("/workdir/tests/data/season_78_2022_round_22.csv", show_col_types = FALSE)
+    expect_equal(obatined_matches, round_22)
+  })
 })
