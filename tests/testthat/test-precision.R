997a403727f58fa8ad7e11e4639416bfe41615e4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-15 21:43:30 -0800

    🛑🧪 Test `summary_from_prediction()`

---
+++ tests/testthat/test-precision.R
@@ -0,0 +1,9 @@
+prediction <- read_csv("/workdir/tests/data/predictions_from_presition.csv", show_col_types = FALSE)
+
+describe("summary", {
+  it("presition", {
+  obtained <- summary_from_prediction(prediction)
+  expected <- tibble::tibble()
+  expect_equal(obtained, expected)
+  })
+})
