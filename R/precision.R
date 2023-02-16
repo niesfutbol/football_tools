8af40546d3284bd00e6783f176cb9587e55b6b22
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-15 21:48:32 -0800

    🪲 Fix bug

--- R/precision.R
+++ R/precision.R
@@ -1,9 +1,9 @@
-summary_from_prediction <- function(prediction) {
+summary_from_prediction <- function(predictions) {
   predictions %>%
-  filter(pred_won != 0) %>%
-  group_by(won) %>%
-  summarize(
-    correct = mean(pred),
-    N = n()
-  )
+    filter(pred_won != 0) %>%
+    group_by(won) %>%
+    summarize(
+      correct = mean(pred),
+      N = n()
+    )
 }
