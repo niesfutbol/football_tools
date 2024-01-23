e6c020f2987f6adeb99c0f733a0c8a4e1b01b403
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-01-22 18:02:16 -0800

    🛑 Test begin date include this day

--- tests/testthat/test-prediction_from_multinom.R
+++ tests/testthat/test-prediction_from_multinom.R
@@ -9,7 +9,7 @@ describe("select_match_to_predict", {
     expect_equal(obatined_matches, round_22)
   })
   it("by date", {
-    obatined_matches <- select_match_to_predict_by_date(season, "2023-01-31", "2023-02-28")
+    obatined_matches <- select_match_to_predict_by_date(season, "2023-02-01", "2023-02-28")
     february <- read_csv("/workdir/tests/data/season_78_2022_february.csv", show_col_types = FALSE)
     expect_equal(obatined_matches, february)
   })
